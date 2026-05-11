787a1abe80e20687a853027d549e799ac8390e1a
Abhinav Pandey <abhinav04.ap@gmail.com>
2026-05-11 23:00:29 +0530

    Add: arranging-coins - submission-2

---
+++ Data Structures & Algorithms/arranging-coins/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        minn=1
+        maxx=n
+        res=1
+        while minn<=maxx:
+            mid=(maxx+minn)//2
+            summ=(mid*(mid+1))//2
+            if summ > n:
+                maxx=mid-1
+            else:
+                res=max(mid,res)
+                minn=mid+1          
+        return res
+
+        
